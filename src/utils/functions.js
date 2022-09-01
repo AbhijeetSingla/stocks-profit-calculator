46b76e8179d817bf893136cd920612ad7269bafb
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-09-01 18:06:21 +0530

    fix: percentage now rounded off

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,11 +2,12 @@ function checkPL(initialValue, quantity, currentAmount) {
     const initialTotal = Number(initialValue) * Number(quantity);
     const currentTotal = Number(currentAmount) * Number(quantity);
     const pl = Math.abs(initialTotal - currentTotal);
+    const percentage = initialValue < currentAmount ? 100 - (currentTotal/initialTotal)*100 : 100 - (initialTotal/currentTotal)*100 
     const object = {
         profit: Number(initialValue) < Number(currentAmount) ? true : false,
         amount: Math.abs(Number(initialValue) - Number(currentAmount)),
         totalAmount: pl,
-        percent: (currentTotal / initialTotal) * 100
+        percent: Math.round((Math.abs(percentage) + Number.EPSILON) * 100) / 100
     }
 
     return object;
